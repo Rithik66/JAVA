7386982df3ab04946078248907c54f572eb40683
Rithik66 <rithikrohith66@gmail.com>
2022-01-22 16:19:42 +0530

    InsertionSort

---
+++ Basics/src/DataStructures/Insertionsort.java
@@ -0,0 +1,19 @@
+package DataStructures;
+
+public class Insertionsort {
+    public static void main(String[] args) {
+        System.out.println(insert(new int[]{5,7,3,4,2,6,1}));
+    }
+    public static int[] insert(int arr[]) {
+        for (int i=1;i<arr.length;i++) {
+            int temp=arr[i];
+            int j=i-1;
+            while(j>=0 && arr[j]>temp){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=temp;
+        }
+        return arr;
+    }
+}
