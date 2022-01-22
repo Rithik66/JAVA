9341d90486630ed64555243345bb77aa013fabee
Rithik66 <rithikrohith66@gmail.com>
2022-01-22 11:04:34 +0530

    Bubble

---
+++ Basics/src/DataStructures/Bubblesort.java
@@ -0,0 +1,19 @@
+package DataStructures;
+
+public class Bubblesort {
+    public static void main(String[] args) {
+    System.out.println(bubble(new int[]{5,7,3,4,2,6,1}));
+    }
+    public static int[] bubble(int arr[]) {
+        for (int i=0;i<arr.length;i++) {
+            for (int j=0;j<arr.length;j++) {
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        return arr;
+    }
+}
