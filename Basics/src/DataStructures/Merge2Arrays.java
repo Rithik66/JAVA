88fb7da0f5ffde6e72ee6ed2b3dce91137a18615
Rithik66 <rithikrohith66@gmail.com>
2022-01-23 09:18:33 +0530

    Commit

--- Basics/src/DataStructures/Merge2Arrays.java
+++ Basics/src/DataStructures/Merge2Arrays.java
@@ -2,7 +2,7 @@ package DataStructures;
 
 import java.util.Arrays;
 
-public class Mergesort {
+public class Merge2Arrays{
     public static void main(String[] args) {
         int ar1[] = {1,3,4,6};
         int ar2[] = {2,5,7,8};
