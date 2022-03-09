416f8995c34f351d48f781df27d93dc0813e1266
Rithik66 <rithikrohith66@gmail.com>
2022-03-09 10:33:45 +0530

    Update

---
+++ SpringBoot/course-api/src/main/java/rit/springboot/starter/hello/HelloController.java
@@ -0,0 +1,12 @@
+package rit.springboot.starter.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@RequestMapping("/hello")
+	public String getAllTopics() {
+		return "hi";
+	}
+}
