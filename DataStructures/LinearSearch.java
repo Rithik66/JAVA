6b7d9cd1a8f8bc6ac5087155067b18aff472bbbe
Rithik <rithikrohith66@gmail.com>
2022-10-08 21:05:01 +0530

    Update

---
+++ DataStructures/LinearSearch.java
@@ -0,0 +1,7 @@
+class LinearSearch{
+    public static void main(String[] args) {
+        int first[] = {1,2,3,4,5,6,7,8,9};
+        String second[] = "rithik".split("");
+        search();
+    }
+}
