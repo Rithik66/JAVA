240acc4b568648c7ee2d325919d6f7f432c79973
Rithik <96978578+Rithik66@users.noreply.github.com>
2024-06-29 12:59:45 +0530

    Update FuncGcd.java

--- Basics/FuncGcd.java
+++ Basics/FuncGcd.java
@@ -1,16 +1,26 @@
-package Javatraining;
+//	TAKE 2 NUMBER
+//	12, 24 
 
+// 	LOGIC:
+//	1.  Find the small number (12)
+//	2.  should divide both numbers from 1 to small number (12)
+//		so divide 12 and 24 with (1,2,3,4,5,6,7,8,9,10,11,12)
+//		if both are divisible store and find the largest divisible number.
+
+
+
+package Javatraining;
 import java.util.Scanner;
 
 public class FuncGcd {
 	static int findValue(int x,int y) {
-		int small=0,large=0;
+		int small=0,ans=0;
 		if(x<y) small=x;
 		else small=y;
 		for(int i=1;i<=small;i++) {
-			if(x%i==0 && y%i==0) large=i;
+			if(x%i==0 && y%i==0) ans=i;
 		}
-		return large;
+		return ans;
 	}
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
